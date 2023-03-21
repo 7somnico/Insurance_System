b83d57755f61742d5698170451003e08050aba5b
amy <yyeo523@aucklanduni.ac.nz>
2023-03-21 20:05:36 +1300

    created Class called Profile

---
+++ src/main/java/nz/ac/auckland/se281/Profile.java
@@ -0,0 +1,18 @@
+package nz.ac.auckland.se281;
+
+public class Profile {
+    private String username;
+    private int age;
+    
+    // creating a constructor to be able to assign variables within a profile
+    public Profile (String username, int age){
+        this.username = username;
+        this.age = age;
+    }
+
+    // printing information (username and age)
+    public String printInfo() {
+        return (this.username + ", " + this.age);
+    } 
+
+}
