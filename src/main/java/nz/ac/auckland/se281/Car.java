5f5e319931fa4e7ffa87337cedad3d4c4a30a5bb
amy <yyeo523@aucklanduni.ac.nz>
2023-04-22 17:06:40 +1200

    checking

--- src/main/java/nz/ac/auckland/se281/Car.java
+++ src/main/java/nz/ac/auckland/se281/Car.java
@@ -1,11 +1,11 @@
 package nz.ac.auckland.se281;
-
 public class Car extends PolicyType {
 
     private String makeAndModel;
     private String licensePlate;
     private int age;
     private double basePremium;
+    private double discountedPremium;
 
     public Car(int sumInsured, String makeAndModel, String licensePlate, String mechanicalBreakdown, int age, String userName) {
         super(sumInsured, userName);
@@ -22,10 +22,42 @@ public class Car extends PolicyType {
         if (mechanicalBreakdown.equals("yes")) {
             this.basePremium = basePremium + 80;
         }
+
+        this.discountedPremium = this.basePremium;
+
     }
-    
+
+    /**
+     * @return the basePremium
+     */
+    @Override
     public int getBasePremium() {
         return (int) basePremium;
+    }
+    
+    /**
+     * @return the makeAndModel
+     */
+    @Override
+    public String getMakeAndModel() {
+        return makeAndModel;
+    }
+
+    /**
+     * set the discountedPremium using the calculated discounted premium
+     * @param calcDiscountedPremium to set the discountedPremium as this value
+     */
+    @Override
+    public void setDiscountedPremium(int calcDiscountedPremium) {
+        this.discountedPremium = calcDiscountedPremium;
+    }
+    
+    /**
+     * @return the discountedPremium
+     */
+    @Override
+    public int getDiscountedPremium() {
+        return (int) this.discountedPremium;
     }
     
     
